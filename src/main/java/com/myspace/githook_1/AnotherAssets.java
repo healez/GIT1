d03d2b7c44e1e4119f551fc29f1105a4179f6d23
workbench <>
2021-04-23 18:23:17 +0800

    {/src/main/java/com/myspace/githook_1/AnotherAssets.java}

--- src/main/java/com/myspace/githook_1/AnotherAssets.java
+++ src/main/java/com/myspace/githook_1/AnotherAssets.java
@@ -10,10 +10,6 @@ public class AnotherAssets implements java.io.Serializable {
 
 	private java.lang.String name;
 
-	private java.lang.String status;
-
-	private java.lang.String state;
-
 	public AnotherAssets() {
 	}
 
@@ -25,27 +21,8 @@ public class AnotherAssets implements java.io.Serializable {
 		this.name = name;
 	}
 
-	public java.lang.String getStatus() {
-		return this.status;
-	}
-
-	public void setStatus(java.lang.String status) {
-		this.status = status;
-	}
-
-	public java.lang.String getState() {
-		return this.state;
-	}
-
-	public void setState(java.lang.String state) {
-		this.state = state;
-	}
-
-	public AnotherAssets(java.lang.String name, java.lang.String status,
-			java.lang.String state) {
+	public AnotherAssets(java.lang.String name) {
 		this.name = name;
-		this.status = status;
-		this.state = state;
 	}
 
 }
