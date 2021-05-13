d9e3fdc11a37e18a4e575b706ca052687e867f0f
erics <>
2021-05-13 12:54:49 +0800

    {/src/main/java/com/myspace/githook_1/ShipmentDataObject.java}

--- src/main/java/com/myspace/githook_1/ShipmentDataObject.java
+++ src/main/java/com/myspace/githook_1/ShipmentDataObject.java
@@ -8,7 +8,6 @@ public class ShipmentDataObject implements java.io.Serializable {
 	static final long serialVersionUID = 1L;
 
 	private java.lang.String countryCode;
-	private java.lang.String countryName;
 	private java.lang.String countryNotes;
 
 	public ShipmentDataObject() {
@@ -20,14 +19,6 @@ public class ShipmentDataObject implements java.io.Serializable {
 
 	public void setCountryCode(java.lang.String countryCode) {
 		this.countryCode = countryCode;
-	}
-
-	public java.lang.String getCountryName() {
-		return this.countryName;
-	}
-
-	public void setCountryName(java.lang.String countryName) {
-		this.countryName = countryName;
 	}
 
 	public java.lang.String getCountryNotes() {
@@ -42,6 +33,12 @@ public class ShipmentDataObject implements java.io.Serializable {
 			java.lang.String countryName, java.lang.String countryName) {
 		this.countryCode = countryCode;
 		this.countryName = countryName;
+		this.countryNotes = countryNotes;
+	}
+
+	public ShipmentDataObject(java.lang.String countryCode,
+			java.lang.String countryNotes) {
+		this.countryCode = countryCode;
 		this.countryNotes = countryNotes;
 	}
 
